09f46b4315aafe07d04deb6dda18c3a01460f56f
Adrian Cortes <ls-adrian@hotmail.com>
2022-03-13 10:37:50 -0500

    We added the ScrollReveal library

--- js/main.js
+++ js/main.js
@@ -2,3 +2,9 @@
 document.querySelector('.btn__menu').addEventListener('click',()=>{
    document.querySelector('.nav__menu').classList.toggle('show')
 })
+
+//ScrollReveal library https://scrollrevealjs.org/
+ScrollReveal().reveal('.header');
+ScrollReveal().reveal('.news', {delay:500});
+ScrollReveal().reveal('.breaking-news', {delay:500});
+ScrollReveal().reveal('.baner-news', {delay:500});
